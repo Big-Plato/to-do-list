eea3b01e6d3d52d42960799fa584e0b432c2b055
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-29 15:51:04 -0300

    imports and tests of the function that create todos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-"use strict";
+// "use strict";
+
+import { createTodo } from "./createTodo.js";
+import { ToDo } from "./todo.js";
+import { Project } from "./project.js";
+
+const rebians = createTodo('Rebians', 'Hilous', '12-07-25', 'High');
+console.log(rebians);
+console.log(rebians.description)
