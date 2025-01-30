13d8e90f5424c9113342b16843063ff597e808e3
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-29 22:22:27 -0300

    adjust the space in showTodo method

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,18 @@
-class toDo {
+export class Todo {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
     }
+
+    showTodo () {
+        return `
+Title: ${this.title}
+Description: ${this.description}
+Date: ${this.dueDate}
+Priority: ${this.priority}
+        `
+    }
+
 }
