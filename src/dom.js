bdebc13c36f66d69cb28491c9977e57d03b08203
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-30 22:44:32 -0300

    implement event listener for the dialog and a function to manipulate input data

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,21 @@
 import { todoSection } from "./index";
 import { Project } from "./project";
+import { Todo } from "./todo";
+
+const addTodo = document.querySelector('.todo-add');
+const addProject = document.querySelector('.project-add');
+const dialog = document.querySelector("#dialog-todo");
+const dialog2 = document.querySelector("#dialog-project")
+const closeDialogBtn = document.querySelector("#close");
+const closeDialogBtn2 = document.querySelector("#close-project");
+const submitBtn = document.querySelector("#submit");
+const submitBtnProject = document.querySelector("#submit-project");
 
 
 export function createTodoCard(todo, project) {
   
+  //Select the proper section of the project you want to add a todo, and then adds
+  //a todo
   const projectSection = selectProjectSection(project);
   console.log(projectSection);
   const todoItem = document.createElement("li");
@@ -13,6 +25,7 @@ export function createTodoCard(todo, project) {
   const inputCheck = document.createElement("input");
   inputCheck.classList.add("mark-todo");
 
+    //Check that will be used in future to finish a todo
     inputCheck.addEventListener('click', () => {
         if (inputCheck.checked) {
             console.log('Yes');
@@ -67,4 +80,40 @@ export function createProjectSection(projectname) {
 function selectProjectSection(projectname) {
     const projectSection = document.querySelector(`.${projectname}`);
     return projectSection;
+}
+
+addTodo.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+addProject.addEventListener("click", () => {
+  dialog2.showModal();
+});
+
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+closeDialogBtn2.addEventListener("click", () => {
+  dialog2.close();
+})
+
+submitBtn.addEventListener("click", (e) => {
+  console.log(e);
+  console.log('HIHIHIHIH')
+  manipulateInputData();
+});
+
+function manipulateInputData () {
+  const title = document.querySelector('#title').value;
+  const description = document.querySelector('#description').value;
+  const date = document.querySelector('#date').value;
+  const check = document.querySelector("#options").value;
+
+  for (let i = 0; i > check.length; i++) {
+    console.log(i);
+  }
+  const projectSection = document.querySelector("#select-project").value
+
+  return [ title, description, date, check ];
 }
