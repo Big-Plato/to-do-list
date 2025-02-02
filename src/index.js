e81a7e6c293b684f8fcc70d42654ebcb8234da32
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-02 18:48:51 -0300

    call the function from local storage after the content is loaded

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
 "use strict";
 
 import "./styles.css";
-import { Todo } from "./modules/todo.js";
+import { Todo } from "./modules/todo.js"
 import { createTodoCard, createProjectSection } from "./modules/dom.js";
-import { finishTodo } from "./modules/finishTodo.js";
-import { retrieveFromLocalStorage } from "./modules/localStorage.js";
+import { retrieveFromLocalStorage } from "./modules/localStorage.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+  retrieveFromLocalStorage();
+})
 
 export const todoSection = document.querySelector(".todo-section");
 
-// const todo1 = new Todo(
-//   "Wash dishes",
-//   "Wash dishes from the yesterday dinner",
-//   "30-01-2025",
-//   "High"
-// );
-// const todo2 = new Todo(
-//   "Study Plato",
-//   "Read Book II of Laws",
-//   "03-02-2025",
-//   "Medium"
-// );
-// const todo3 = new Todo("Market", "Go to market", "05-02-2025", "High");
+const todo1 = new Todo("Wash dishes", "Wash dishes from the yesterday dinner", "30-01-2025", "High");
+const todo2 = new Todo(
+  "Study Plato",
+  "Read Book II of Laws",
+  "03-02-2025",
+  "Medium"
+);
+const todo3 = new Todo("Market", "Go to market", "05-02-2025", "High");
 
-// createProjectSection("Default");
-// createProjectSection("Completed");
+createProjectSection('Default');
+createProjectSection('Completed'); 
 
-// createTodoCard(todo1, "Default");
-// createTodoCard(todo2, "Default");
-// createTodoCard(todo3, "Default");
+createTodoCard(todo1, 'Default');
+createTodoCard(todo2, 'Default');
+createTodoCard(todo3, 'Default');
 
-export function projectInterval(projectname) {
+export function projectInterval (projectname) {
   const select = document.querySelector("#select-project");
   const option = document.createElement("option");
   option.textContent = projectname;
   select.appendChild(option);
 }
+
+
 
 /* Processos
 
