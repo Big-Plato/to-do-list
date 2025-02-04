8d3e40cc5617697065292d404bd5f193fc6698a2
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-04 19:22:32 -0300

    almost finish complete section

--- src/modules/finishTodo.js
+++ src/modules/finishTodo.js
@@ -1,12 +1,15 @@
 import { todoSection } from "../index";
+import { createProjectSection } from "./dom";
 
 export const completed = document.querySelector(".finish");
 
 
 export function completeSection () {
-    todoSection.style.display = "none";   
+    todoSection.innerHTML = ""; 
+    const project = document.createElement("div");
+    project.classList.add("project");
+    project.textContent = "Completed";
+    todoSection.appendChild(project);  
 }
 
-export function completeTodo () {
-    
-}
+
