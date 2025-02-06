8fede57433aa8c891e8fb0b43f971df1aa4733e8
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-05 21:44:57 -0300

    erase the import

--- src/modules/finishTodo.js
+++ src/modules/finishTodo.js
@@ -3,13 +3,10 @@ import { createProjectSection } from "./dom";
 
 export const completed = document.querySelector(".finish");
 
-
-export function completeSection () {
-    todoSection.innerHTML = ""; 
-    const project = document.createElement("div");
-    project.classList.add("project");
-    project.textContent = "Completed";
-    todoSection.appendChild(project);  
+export function completeSection() {
+  todoSection.innerHTML = "";
+  const project = document.createElement("div");
+  project.classList.add("project");
+  project.textContent = "Completed";
+  todoSection.appendChild(project);
 }
-
-
