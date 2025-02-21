af24a6013e92da5eea5bb582ec6d5668ffb04175
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-20 22:10:27 -0300

    add: function that handles the change of section when a todo is marked

--- src/modules/finishTodo.js
+++ src/modules/finishTodo.js
@@ -3,9 +3,25 @@ import { todoSection } from "../index";
 export const completed = document.querySelector(".finish");
 
 export function completeSection() {
-  todoSection.innerHTML = "";
   const project = document.createElement("div");
   project.classList.add("project");
   project.textContent = "Completed";
   todoSection.appendChild(project);
 }
+
+export const sendToCompleted = (todoItem, todo) => {
+  const todoProject = todo.project;
+
+  const send = (todoDiv) => {
+    const completed = document.querySelector(".Completed");
+    completed.appendChild(todoDiv);
+  }
+
+  const retrieve = (todoDiv) => {
+    const retrieved = document.querySelector(`.${todoProject}`);
+    console.log(retrieved)
+    retrieved.appendChild(todoDiv);
+  }
+  
+  return { send, retrieve }
+} 
