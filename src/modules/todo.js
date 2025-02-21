4cdc0a26d0b1bfa3594bc6092d3bc42af601737d
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-20 22:10:42 -0300

    fix: setter

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,25 +1,22 @@
 export class Todo {
-    constructor(title, description, dueDate, priority, project) {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.project = project;
+  constructor(title, description, dueDate, priority, project) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.project = project;
+  }
 
-    }
+    get showTodo() {
+    return `
+    Title: ${this.title}
+    Description: ${this.description}
+    Date: ${this.dueDate}
+    Priority: ${this.priority}
+        `;
+  }
 
-    showTodo () {
-        return `
-Title: ${this.title}
-Description: ${this.description}
-Date: ${this.dueDate}
-Priority: ${this.priority}
-        `
-    }
-
-    set props (project) {
-        this.project = project;
-        return project;
-    }
-
+    set projectName(projectName) {
+    this.project = projectName;
+  }
 }
